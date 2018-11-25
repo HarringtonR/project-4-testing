851b7fd3c527d8d7e5e3be4d3da674a998158853
HarringtonR <ross.ludeke@gmail.com>
2018-11-24 21:15:22 -0500

    heroku testing

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(logger('dev'));
+// app.use(express.static('public'));
 app.use(express.static('p4/build'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,8 +19,8 @@ app.listen(PORT, () => {
 const chatroom = require('./routes/routes');
 app.use('/videoPage', chatroom);
 
-// const welcome = require('./routes/routes');
-// app.use('/Welcome', welcome);
+const welcome = require('./routes/routes');
+app.use('/Welcome', welcome);
 
 app.use('*', (req, res) => {
   res.status(400).json({
